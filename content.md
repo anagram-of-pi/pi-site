b2c8d80c07fa0c378149b908d71b30ee6333350a
anagram-of-pi <58116783+anagram-of-pi@users.noreply.github.com>
2026-09-23 19:46:50 -0700

    More content + styling

---
+++ content.md
@@ -0,0 +1,15 @@
+# 3.14159265359
+
+## What is π?
+
+## Where does π come from?
+## How do we approximate π?
+
+<!-- # What does it mean for π to be irrational? -->
+
+## Why does π show up everywhere?
+## What do we know about π?
+## What don't we know about π?
+
+
+## Why do we need to know about π?
--- src/script.js
+++ src/script.js
@@ -1,3 +1,29 @@
 document.addEventListener("mousemove", (e) => {
-    
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    if (Number.isNaN(mouseX)) {
+        console.log(e)
+    }
+    let cursor = document.getElementById("cursor");
+    cursor.style.setProperty("--pos-x", mouseX + "px");
+    cursor.style.setProperty("--pos-y", mouseY + "px");
 })
+
+
+// Check if page has been scrolled in three seconds
+hasScrolled = false;
+
+function handleScroll() {
+    hasScrolled = true;
+    window.removeEventListener("scroll", handleScroll);
+}
+
+window.addEventListener("scroll", handleScroll);
+
+setTimeout(() => {
+    if (!hasScrolled) {
+        window.removeEventListener("scroll", handleScroll);
+        
+        document.getElementById("what-is").scrollIntoView({ behavior: "smooth" });
+    }
+}, 2000);
